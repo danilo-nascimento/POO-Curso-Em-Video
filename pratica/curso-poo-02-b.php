8e277fe12542197e7ae247a0c5b7a24ebc689f02
Danilo <danilo.nascimento.webdesign@gmail.com>
2018-06-01 01:46:13 -0300

    Create and Add file curso-poo-02-b.php

---
+++ pratica/curso-poo-02-b.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Curso POO PHP – 02b – Criando Classes e Objetos em PHP</title>
+</head>
+<body>
+    <h1>Curso POO PHP – 02b – Criando Classes e Objetos em PHP</h1>
+    
+</body>
+</html>
