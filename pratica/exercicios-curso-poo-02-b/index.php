9387e125cecc1f076ce5fc503163573d7ddaaa3b
Danilo <danilo.nascimento.webdesign@gmail.com>
2018-06-01 07:55:26 -0300

    Add class to curso-poo-02-b.php; add dir exercicios-curso-poo-02-b and exercicios-curso-poo-02-b/index.php

--- pratica/exercicios-curso-poo-02-b/index.php
+++ pratica/exercicios-curso-poo-02-b/index.php
@@ -3,36 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <title>Curso POO PHP – 02b – Criando Classes e Objetos em PHP</title>
+    <title>Exercícios | Curso POO PHP – 02b – Criando Classes e Objetos em PHP</title>
 </head>
 <body>
-    <h1>Curso POO PHP – 02b – Criando Classes e Objetos em PHP</h1>
-    <?php
-        require_once('classes/Caneta.php');
-        $c1 = new Caneta;
-        $c1->cor = "Azul" ;
-        $c1->ponta = 0.5;
-        $c1->tampada = true;
-
-        $c1->destampar();
-        $c1->rabiscar();
-
-        $c2 = new Caneta;
-        $c2->cor = "Verde" ;
-        $c2->carga = 50;
-        $c2->tampada = true;
-
-        $c2->tampar();
-        $c2->rabiscar();
-    ?>
+    <h1>Exercícios | Curso POO PHP – 02b – Criando Classes e Objetos em PHP</h1>
     
-    <pre>
-        <?php print_r($c1); ?>
-    </pre>
-
-    <pre>
-        <?php print_r($c2); ?>
-    </pre>
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
